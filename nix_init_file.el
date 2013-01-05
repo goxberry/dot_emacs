0949830fdd9e2a95a4eb3f9cb2c17ac14701934a
Geoffrey Oxberry <oxberry1@llnl.gov>
2013-01-04 16:08:12 -0800

    Moved init_file.el to nix_init_file.el.
    Rename reflects that it's only appropriate for UNIX-like OSes.

---
+++ nix_init_file.el
@@ -0,0 +1,12 @@
+;; Symlink this file to "~/.emacs". In this directory, you would use
+;; "ln -s init_file.el ~/.emacs". Your current directory should be 
+;; "~/.emacs.d".
+
+;; In order to make this file nice and tidy (and compatible with both
+;; OS X and Linux without doing anything too too complicated!) just
+;; load from ~/.emacs.d (obviously, in Windows, the directory will be
+;; something different...
+(load "~/.emacs.d/emacs_init.el")
+
+;; TODO(goxberry@gmail.com): Make this load command more Windows-
+;; compatible!
